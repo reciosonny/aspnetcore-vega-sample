19b48137e204eaf356002026c0677e9ffc3ac9e6
Sonny Recio <reciosonny@gmail.com>
2017-08-10 10:38:08 +0800

    Included underscore package

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -25,7 +25,6 @@ namespace aspnetcore_vega_sample.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateVehicle([FromBody] SaveVehicleResource vehicleResource)
         {
-            throw new Exception();
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
